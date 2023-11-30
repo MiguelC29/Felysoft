d73c8d7a9e436b76229a2681c1090c72df34dab2
devsan-g3 <santimahe001@gmail.com>
2023-11-29 20:46:51 -0600

    dataTables servicios & tiposervicios

--- app/vistas/paginas/servicios/inicio.php
+++ app/vistas/paginas/servicios/inicio.php
@@ -1,38 +1,45 @@
 <?php require RUTA_APP . '/vistas/inc/header.php'; ?>
 <a href="<?php echo RUTA_URL; ?>servicios/agregar" class="btn btn-primary">Insertar</a>
 <a href="<?php echo RUTA_URL; ?>" class="btn btn-primary">Volver</a>
-
-<table class="table text-center">
-    <thead>
-        <tr>
-            <th>ID de Servicio</th>
-            <th>Estado</th>
-            <th>Fecha de Creación</th>
-            <th>Fecha de Modificación</th>
-            <th>Precio Adicional</th>
-            <th>Total</th>
-            <th>Tipo de Servicio</th>
-            <th>Acciones</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($datos['servicios'] as $servicio) : ?>
-            <tr>
-                <td><?php echo $servicio->pkIdServicio; ?></td>
-                <td><?php echo $servicio->estado; ?></td>
-                <td><?php echo $servicio->fechaCreacion; ?></td>
-                <td><?php echo $servicio->fechaModificacion; ?></td>
-                <td><?php echo $servicio->precioAdicional; ?></td>
-                <td><?php echo $servicio->total; ?></td>
-                <td><?php echo $servicio->nombreTipoServicio; ?></td>
-                <td>
-                    <a href="<?php echo RUTA_URL; ?>servicios/editar/<?php echo $servicio->pkIdServicio; ?>"
-                        class="btn btn-warning">Editar</a>
-                    <a href="<?php echo RUTA_URL; ?>servicios/borrar/<?php echo $servicio->pkIdServicio; ?>"
-                        class="btn btn-danger">Borrar</a>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="table-responsive">
+                <table id="TablesFelysoft" class="table table-bordered table-hover text-center">
+                    <thead>
+                        <tr>
+                            <th>ID de Servicio</th>
+                            <th>Estado</th>
+                            <th>Fecha de Creación</th>
+                            <th>Fecha de Modificación</th>
+                            <th>Precio Adicional</th>
+                            <th>Total</th>
+                            <th>Tipo de Servicio</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($datos['servicios'] as $servicio) : ?>
+                            <tr>
+                                <td><?php echo $servicio->pkIdServicio; ?></td>
+                                <td><?php echo $servicio->estado; ?></td>
+                                <td><?php echo $servicio->fechaCreacion; ?></td>
+                                <td><?php echo $servicio->fechaModificacion; ?></td>
+                                <td><?php echo $servicio->precioAdicional; ?></td>
+                                <td><?php echo $servicio->total; ?></td>
+                                <td><?php echo $servicio->nombreTipoServicio; ?></td>
+                                <td>
+                                    <a href="<?php echo RUTA_URL; ?>servicios/editar/<?php echo $servicio->pkIdServicio; ?>"
+                                        class="btn btn-warning">Editar</a>
+                                    <a href="<?php echo RUTA_URL; ?>servicios/borrar/<?php echo $servicio->pkIdServicio; ?>"
+                                        class="btn btn-danger">Borrar</a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 <?php require RUTA_APP . '/vistas/inc/footer.php'; ?>
--- app/vistas/paginas/tiposervicios/inicio.php
+++ app/vistas/paginas/tiposervicios/inicio.php
@@ -1,31 +1,39 @@
 <?php require RUTA_APP . '/vistas/inc/header.php';?>
 <a href="tiposervicios/agregar" class="btn btn-primary">Insertar</a>
 <a href="<?php echo RUTA_URL?>" class="btn btn-primary">Volver</a>
-<table class="table text-center">
-    <thead>
-        <tr>
-            <th>N. de Servicio</th>
-            <th>Nombre</th>
-            <th>Descripción</th>
-            <th>Precio</th>
-            <th>Acciones</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach($datos['tiposervicios'] as $tiposervicio) : ?>
-        <tr>
-            <td><?php echo $tiposervicio->idTipoServicio; ?></td>
-            <td><?php echo $tiposervicio->nombre; ?></td>
-            <td><?php echo $tiposervicio->descripcion; ?></td>
-            <td><?php echo $tiposervicio->precio; ?></td>
-            <td>
-                <a href="<?php echo RUTA_URL; ?>tiposervicios/editar/<?php echo $tiposervicio->idTipoServicio; ?>"
-                    class="btn btn-warning">Editar</a>
-                <a href="<?php echo RUTA_URL; ?>tiposervicios/borrar/<?php echo $tiposervicio->idTipoServicio; ?>"
-                    class="btn btn-danger">Borrar</a>
-            </td>
-        </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="table-responsive">
+                <table id="TablesFelysoft" class="table table-bordered table-hover text-center">
+                    <thead>
+                        <tr>
+                            <th>N. de Servicio</th>
+                            <th>Nombre</th>
+                            <th>Descripción</th>
+                            <th>Precio</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($datos['tiposervicios'] as $tiposervicio) : ?>
+                        <tr>
+                            <td><?php echo $tiposervicio->idTipoServicio; ?></td>
+                            <td><?php echo $tiposervicio->nombre; ?></td>
+                            <td><?php echo $tiposervicio->descripcion; ?></td>
+                            <td><?php echo $tiposervicio->precio; ?></td>
+                            <td>
+                                <a href="<?php echo RUTA_URL; ?>tiposervicios/editar/<?php echo $tiposervicio->idTipoServicio; ?>"
+                                    class="btn btn-warning">Editar</a>
+                                <a href="<?php echo RUTA_URL; ?>tiposervicios/borrar/<?php echo $tiposervicio->idTipoServicio; ?>"
+                                    class="btn btn-danger">Borrar</a>
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 <?php require RUTA_APP . '/vistas/inc/footer.php';?>
